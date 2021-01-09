8552eeaef824aac9bf248d1b72ff6791052625d7
Lukas Planz <lukas.planz@web.de>
2021-01-09 14:04:33 +0100

    Added MessageDigest getter to MavenChecksum enum

--- resolver/src/main/java/de/md5lukas/maven/resolver/MavenChecksum.java
+++ resolver/src/main/java/de/md5lukas/maven/resolver/MavenChecksum.java
@@ -2,15 +2,33 @@ package de.md5lukas.maven.resolver;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @RequiredArgsConstructor
 public enum MavenChecksum {
 
-    MD5("md5"),
-    SHA1("sha1");
+    MD5("md5", () -> MessageDigest.getInstance("MD5")),
+    SHA1("sha1", () -> MessageDigest.getInstance("SHA-1"));
 
     @Getter
     @NotNull
     private final String extensionAppendix;
+
+    @NotNull
+    private final MessageDigestSupplier messageDigestSupplier;
+
+    @SneakyThrows // MD5 and SHA-1 are present
+    @NotNull
+    public MessageDigest getMessageDigest() {
+        return messageDigestSupplier.get();
+    }
+
+    @FunctionalInterface
+    private interface MessageDigestSupplier {
+        MessageDigest get() throws NoSuchAlgorithmException;
+    }
 }
